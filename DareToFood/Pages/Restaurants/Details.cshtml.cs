2919680587da94f8f20364cde644ef1d7ed23e5d
Marek Herej <marek.herej@outlook.com>
2019-09-11 13:50:25 +0100

    Details page added.

---
+++ DareToFood/Pages/Restaurants/Details.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DareToFood.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DareToFood.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+
+        public void OnGet()
+        {
+            Restaurant = new Restaurant();
+
+        }
+    }
+}
